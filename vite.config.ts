3b975c132b25821eb8a33ce37c3dfa19e14ba063
eyalyehiely <eyalwork0@gmail.com>
2024-11-18 02:29:59 +0700

    update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,10 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: parseInt(process.env.PORT as string) || 4173, // Use the port set by Railway or default to 4173
+    host: true, // Expose the server to the network
+  },
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
